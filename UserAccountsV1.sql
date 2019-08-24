8c913d27ee66456aaaa2d004cdfa3690f4c1fef9
Max Owens <42765628+MOwneZ@users.noreply.github.com>
2019-08-24 17:10:08 +1200

    Add files via upload

--- UserAccountsV1.sql
+++ UserAccountsV1.sql
@@ -1,19 +1,32 @@
+drop database UserAccounts;
 create database UserAccounts;
 use UserAccounts;
 
-create table Registration(
-userID int primary key not null auto_increment,
-firstName char(25),
-lastName char(25),
-email varchar(50),
-userName varchar(25),
-userPassword varchar(30)
-
+create table UserAccount (
+userName varchar (25),
+userPassword varchar (32),
+primary key (userName)
 )engine innodb;
 
-create table Login(
-loginID int primary key not null auto_increment,
-userName varchar(25),
-userPassword varchar(30),
-foreign key (userName, userPassword) references Registration(userName, userPassword)
+create table AccountDetails (
+accountID integer auto_increment,
+firstName char (40),
+lastName char (40),
+userName varchar (25),
+emailAddress varchar (30),
+dateOfBirth date,
+phoneNo int,
+address1 varchar (50),
+address2 varchar (50),
+primary key (accountID),
+foreign key (userName) references UserAccount(userName)
+)engine innodb;
+
+create table BlogPost (
+postID integer auto_increment,
+postTitle varchar (30),
+postContent varchar (500),
+userName varchar (25),
+primary key (postID),
+foreign key (userName) references UserAccount (userName)
 )engine innodb;
