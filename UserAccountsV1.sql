6132cce9e1fa47e6c9793e1a6896b83770f4ab86
Max Owens <42765628+MOwneZ@users.noreply.github.com>
2019-08-30 11:29:30 +1200

    Add files via upload

--- UserAccountsV1.sql
+++ UserAccountsV1.sql
@@ -1,37 +1,40 @@
 SET SQL_SAFE_UPDATES = 0;
 drop database Accounts;
-create database Accounts;
+create database if not exists Accounts;
 use Accounts;
 
 CREATE TABLE AccountDetails (
     accountID INTEGER AUTO_INCREMENT,
-    firstName CHAR(40),
-    lastName CHAR(40),
-    emailAddress VARCHAR(30),
-    dateOfBirth DATE,
-    phoneNo INTEGER,
-    address1 VARCHAR(50),
-    address2 VARCHAR(50),
+    firstName CHAR(40) NOT NULL,
+    lastName CHAR(40) NOT NULL,
+    userName VARCHAR(25) UNIQUE,
+    userPassword VARCHAR(32) UNIQUE,
+    emailAddress VARCHAR(30) UNIQUE,
+    dateOfBirth DATE NOT NULL,
+    phoneNo INTEGER NOT NULL,
+    address1 VARCHAR(50) NOT NULL,
+    address2 VARCHAR(50) NOT NULL,
     PRIMARY KEY (accountID)
 )  ENGINE INNODB;
 
 CREATE TABLE UserAccount (
-    userID INTEGER AUTO_INCREMENT,
-    userName VARCHAR(25),
-    userPassword VARCHAR(32),
-    accountID INTEGER,
-    UNIQUE KEY (userName),
-    PRIMARY KEY (userID),
+    userName VARCHAR(25) UNIQUE NOT NULL,
+    userPassword VARCHAR(32) UNIQUE NOT NULL,
+    accountID INTEGER NOT NULL,
+    PRIMARY KEY (userName),
     FOREIGN KEY (accountID)
-        REFERENCES AccountDetails (accountID)
+        REFERENCES AccountDetails (accountID),
+    FOREIGN KEY (userName)
+        REFERENCES AccountDetails (userName),
+    FOREIGN KEY (userPassword)
+        REFERENCES AccountDetails (userPassword)
 )  ENGINE INNODB;
 
 CREATE TABLE BlogPost (
     postID INTEGER AUTO_INCREMENT,
-    postTitle VARCHAR(30),
-    postContent VARCHAR(500),
-    userName VARCHAR(25),
-    UNIQUE KEY (userName),
+    postTitle VARCHAR(30) NOT NULL,
+    postContent VARCHAR(500) NOT NULL,
+    userName VARCHAR(25) UNIQUE NOT NULL,
     PRIMARY KEY (postID),
     FOREIGN KEY (userName)
         REFERENCES UserAccount (userName)
@@ -39,10 +42,9 @@ CREATE TABLE BlogPost (
 
 CREATE TABLE BlogComment (
     commentID INTEGER AUTO_INCREMENT,
-    postID INTEGER,
-    commentContent VARCHAR(300),
-    userName VARCHAR(25),
-	UNIQUE KEY (userName),
+    postID INTEGER NOT NULL,
+    commentContent VARCHAR(300) NOT NULL,
+    userName VARCHAR(25) UNIQUE NOT NULL,
     PRIMARY KEY (commentID),
     FOREIGN KEY (postID)
         REFERENCES BlogPost (postID),
@@ -50,33 +52,38 @@ CREATE TABLE BlogComment (
         REFERENCES UserAccount (userName)
 )  ENGINE INNODB;
 
- -- first creating an account with username and password. Both of these forms will be entered at the same time, so something will exist to update the IDs.
+ -- first creating an account complete with information and with username and password. 
+ -- update trigger which creates a simple user login table, for bridging with the blog post/comment tables
+ 
+ drop trigger CreateUserLogin;
+ 
+ delimiter $$
+ create trigger CreateUserLogin after insert on AccountDetails
+ for each row
+ begin 
+ insert into UserAccount values (new.userName, new.userPassword, new.AccountID);
+ end$$
+ delimiter ;
+ 
+insert into AccountDetails values (null,"Amit","Sarkar","AmitTheSlayer6969","password3","AmitLordOfTheSun@NZGardner.com",'1000-12-25',033352456,"34 Kingly Street, Calimara","PO Box Fishman"); 
+insert into AccountDetails values (null,"Nick","Leslie","SnickerMan","ImBald","Glenda12@NZGardner.com",'2004-01-11',033453623,"University of Canterbury","PO Box UC"); 
+insert into AccountDetails values (null,"Emily","Chuck 'E' Cheese","WheresMySuperSuit","Fibbonacci","erangleMan@gmail.com",'1997-05-22',034968394,"8 Newsons Road","RD3 Cheviot"); 
+insert into AccountDetails values (null,"Spup","M'larky","MrKansas","flubber","FlatEarthSociety@FESAdmin.com",'1990-05-04',035903456,"NASA","Miami, FL"); 
 
-insert into AccountDetails values (null,"Amit","Sarkar","AmitLordOfTheSun@NZGardner.com",'1000-12-25',033352456,"34 Kingly Street, Calimara","PO Box Fishman"); 
-insert into AccountDetails values (null,"Nick","Leslie","Glenda12@NZGardner.com",'2004-01-11',033453623,"University of Canterbury","PO Box UC"); 
-insert into AccountDetails values (null,"Emily","Chuck 'E' Cheese","erangleMan@gmail.com",'1997-05-22',034968394,"8 Newsons Road","RD3 Cheviot"); 
-insert into AccountDetails values (null,"Spup","M'larky","FlatEarthSociety@FESAdmin.com",'1990-05-04',035903456,"NASA","Miami, FL"); 
 
-insert into UserAccount values (Null,"AmitTheSlayer6969","thebestpassword",null);
-insert into UserAccount values (Null,"MrPoolCleaner","anevenb3tterpassword",null);
-insert into UserAccount values (Null,"Wif.e","wheredidallmyteago",null);
-insert into UserAccount values (Null,"CaptainClatcher","ohIfounditileftitonthetable",null);
     
-SELECT 
-	*
-FROM
-	AccountDetails;
+ -- 
+ 
+    
+    
 
-SELECT 
-    *
-FROM
-    UserAccount;
-    
- -- update query which allocates the UserAccount to it's appropriate account ID. will be created at same time, so no worries
-    
-UPDATE UserAccount
+/*UPDATE UserAccount
 inner join AccountDetails on UserAccount.userID = AccountDetails.AccountID
 SET 
     UserAccount.accountID = AccountDetails.accountID
 WHERE
-    UserAccount.userID = AccountDetails.accountID;
+    UserAccount.userID = AccountDetails.accountID;*/
+    
+    
+    
+    
